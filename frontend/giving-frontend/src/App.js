31dd3941e5283df75b439eb92018aeb9d8388ca9
arinale <73424915+arinale@users.noreply.github.com>
2024-03-08 18:54:21 +0200

    add profile photo

--- frontend/giving-frontend/src/App.js
+++ frontend/giving-frontend/src/App.js
@@ -2,15 +2,26 @@ import "./App.css";
 import RegistrationForm from "./components/RegistrationForm";
 import Login from "./components/Login/Login";
 
+import { Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
+  const isLoggedIn = true;
   return (
     <div className="App">
       <div>
         <h1>My App</h1>
-
-        <Login />
-        {/*<RegistrationForm /> */}
       </div>
+      {/* <Login />
+      <RegistrationForm /> */}
+
+      <Router>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={RegistrationForm} />
+        <Route path="/login">
+          {isLoggedIn ? <Redirect to="/home" /> : <Login />}
+        </Route>
+      </Router>
     </div>
   );
 }
